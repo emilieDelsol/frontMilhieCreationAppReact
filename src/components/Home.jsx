297e72ec8a2a3dcb747bde93400d79e6451c4b59
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-18 15:52:48 +0100

    add axios in life tree

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+
 import { Card, Container} from 'react-bootstrap';
 import products from './products/Products';
 function Home() {
 
     return (
-        <div>
+        <Container fluid >
 
         <h1>Home</h1>
-        <Container fluid className="d-flex flex-wrap m-4">
+        <Container fluid className="d-flex flex-wrap m-auto">
 
         {products.map((product) => (
             <Card className="bg-dark text-white m-4"style={{ width: '18rem', height:'auto'}}>
-                <Card.Img src={product.image} alt="" />
-                <Card.ImgOverlay>
-                    
+                <Card.Img src={product.image} alt="" style={{ width: '100%'}} className="m-auto"/>
+                <Card.ImgOverlay className="w-100">
                     <Card.Title class="btn m-auto" style={{color:'white', backgroundColor:'rgba(0,0,0,0.5)'}} >{product.title}</Card.Title>
                 </Card.ImgOverlay>
             </Card>
-        ))};
+        ))}
         </Container>
-        </div>
+        </Container>
 
     );}
 
--- src/components/LifeTree.jsx
+++ src/components/LifeTree.jsx
@@ -1,23 +1,66 @@
-import { Card, Button} from 'react-bootstrap';
+import { Card, Button, Carousel, Container} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import axios from "axios";
 
 function LifeTree() {
-
+    const [LifeTrees, setLifeTrees] = useState([])
+    const url = "https://localhost:44321/MilhieCreation/Category/1" 
+    // axios request to get tournaments
+    const getLifeTrees =()=>{
+      axios
+          .get(url)
+          .then((res)=> setLifeTrees(res.data))
+          .catch((error) => {
+              console.error(error);
+            });   
+  }
+    useEffect(() => {
+      getLifeTrees();
+    }, [])
     return (
-        <div>
-            <h1>Life tree</h1>
+        <Container fluid className="m-0">
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <h1>Arbres de vie</h1>
+        <Container fluid className="d-flex flex-wrap justify-content-center">
+            {LifeTrees.map(item =>
+          
+          <Card className="m-0 m-lg-5" id="cardItem" >
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>{item.disponibility ? "Disponible" : "Sur commande"}</Card.Text>
+                
+                <Carousel className="m-4" interval='10000'slide='false'>
+               {item.images.map(image =>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={image}
+                    alt="slide"
+                    />
+                    
+                </Carousel.Item>
+                )}
+                </Carousel>
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
                     <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                        <p>
+                            {item.description}
+                        </p>
+                        <p>
+                            Prix: {item.price}€
+                        </p>
+                        <p>{item.disponibility ? "Disponible" : "Sur commande"}</p>
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Button variant="primary" href={item.link}>link </Button>
                 </Card.Body>
-                </Card>
-        </div>
+                <Card.Footer>
+                    {item.reference}
+                </Card.Footer>
+            </Card>
+        )}
+        </Container>
+        </Container>
     );}
 
 
